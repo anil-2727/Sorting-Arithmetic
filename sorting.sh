79e111dd13c0efb24dc1eca5388e2b2a3bd1499f
anil-2727 <bali39959@gmail.com>
2021-06-05 19:31:07 +0530

    Added: dictionory-array

--- sorting.sh
+++ sorting.sh
@@ -15,19 +15,23 @@ i=0
 compute1=$(($a+$(($b*$c))))
 echo "result of a+b*c :$compute1"
 arith[$i]=$compute1
+array[i]=${arith[$i]}
 ((i++))
 
 compute2=$(($c+$(($a*$b))))
 echo "result of a*b+c :$compute2"
 arith[$i]=$compute2
+array[i]=${arith[$i]}
 ((i++))
 
 compute3=$(($c+$(($a/$b))))
 echo "result of c+a/b :$compute3"
 arith[$i]=$compute3
+array[i]=${arith[$i]}
 ((i++))
 
 compute4=$(($c+$(($a%$b))))
 echo "result of a%b+c :$compute4"
 arith[$i]=$compute4
+array[i]=${arith[$i]}
 ((i++))
